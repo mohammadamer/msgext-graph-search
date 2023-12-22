135c61d34c7f1c5ca59593a431f1f5678ca6f19a
mohammadamer <m.amer@live.com>
2023-12-22 20:53:21 +0000

    Message Extension commands

---
+++ src/enums/CommandIds.ts
@@ -0,0 +1,7 @@
+export enum CommandIds {
+  SearchEvents = 'SearchEvents',
+  SearchFiles = 'SearchFiles',
+  SearchListItems = 'SearchListItems',
+  SearchMessages = 'SearchMessages',
+  SearchNews = 'SearchNews',
+}
